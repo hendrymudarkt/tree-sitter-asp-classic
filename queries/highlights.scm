c4fffc324411df34bfffd19a7b5e2ac8a83995be
hendry <hendry@email.com>
2026-07-08 11:28:46 +0700

    fix: use theme-safe capture names for highlighting

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -2,37 +2,43 @@
 ; Grammar is token-oriented (not a full statement AST), so keywords,
 ; built-in objects, and built-in functions are all distinguished from
 ; plain identifiers via regex match here.
+;
+; Zed resolves capture names right-to-left against the active theme: it
+; tries the rightmost capture first, and falls back to the next one to the
+; left if the theme doesn't define a style for it. We use common, widely
+; supported capture names (@keyword, @string, @number, @comment, @function,
+; @type, @variable, @constant, @operator, @punctuation) to make sure this
+; actually renders with color in most themes, rather than more obscure
+; captures like @function.builtin or @property that many themes leave
+; unstyled (falling back to plain foreground/white).
 
 ; === ASP Delimiters ===
-(asp_directive) @tag
-(asp_expression) @_asp_expr
-(asp_block) @_asp_block
+"<%" @punctuation.special
+"%>" @punctuation.special
+"<%=" @punctuation.special
+"<%@" @punctuation.special
 
 ; === VBScript Keywords ===
 ((identifier) @keyword
   (#match? @keyword "^(?i)(Dim|Set|If|Then|Else|ElseIf|End|For|Each|In|To|Step|Next|While|Wend|Do|Loop|Until|Select|Case|Sub|Function|With|Exit|On|Error|Resume|GoTo|Call|New|Public|Private|Const|ReDim|Preserve|ByVal|ByRef|Optional|ParamArray|Class|Property|Get|Let|TypeOf|Execute|ExecuteGlobal|Option|Explicit|Rem|Is|Like|Mod|Not|And|Or|Xor|Eqv|Imp)$"))
 
+; === Boolean / Special literals (matched as identifiers) ===
+((identifier) @constant
+  (#match? @constant "^(?i)(True|False|Null|Nothing|Empty)$"))
+
 ; === ASP Built-in Objects ===
-((identifier) @type.builtin
-  (#match? @type.builtin "^(?i)(Response|Request|Server|Session|Application|ObjectContext|ASPError)$"))
+((identifier) @type
+  (#match? @type "^(?i)(Response|Request|Server|Session|Application|ObjectContext|ASPError)$"))
 
-; === ASP Collections / Properties ===
-((identifier) @property
-  (#match? @property "^(?i)(Form|QueryString|Cookies|ServerVariables|Files|Contents|StaticObjects|Count|Item|Key)$"))
-
-; === VBScript Built-in Functions ===
-((identifier) @function.builtin
-  (#match? @function.builtin "^(?i)(Abs|Array|Asc|Atn|CBool|CByte|CCur|CDate|CDbl|Chr|CInt|CLng|Cos|CreateObject|CSng|CStr|Date|DateAdd|DateDiff|DatePart|DateSerial|DateValue|Day|Erase|Exp|Filter|Fix|FormatCurrency|FormatDateTime|FormatNumber|FormatPercent|GetObject|Hex|Hour|InStr|InStrRev|Int|IsArray|IsDate|IsEmpty|IsNull|IsNumeric|IsObject|Join|LBound|LCase|Left|Len|Log|LTrim|Mid|Minute|Month|MonthName|Now|Oct|Replace|Right|Rnd|Round|RTrim|Second|Sgn|Sin|Space|Split|Sqr|StrComp|StrReverse|String|Tan|Time|Timer|TimeSerial|TimeValue|Trim|TypeName|UBound|UCase|VarType|Weekday|WeekdayName|Year|CVar|Val|Write|Redirect|MapPath|HTMLEncode|URLEncode)$"))
+; === VBScript / ASP Built-in Functions & Methods ===
+((identifier) @function
+  (#match? @function "^(?i)(Abs|Array|Asc|Atn|CBool|CByte|CCur|CDate|CDbl|Chr|CInt|CLng|Cos|CreateObject|CSng|CStr|Date|DateAdd|DateDiff|DatePart|DateSerial|DateValue|Day|Erase|Exp|Filter|Fix|FormatCurrency|FormatDateTime|FormatNumber|FormatPercent|GetObject|Hex|Hour|InStr|InStrRev|Int|IsArray|IsDate|IsEmpty|IsNull|IsNumeric|IsObject|Join|LBound|LCase|Left|Len|Log|LTrim|Mid|Minute|Month|MonthName|Now|Oct|Replace|Right|Rnd|Round|RTrim|Second|Sgn|Sin|Space|Split|Sqr|StrComp|StrReverse|String|Tan|Time|Timer|TimeSerial|TimeValue|Trim|TypeName|UBound|UCase|VarType|Weekday|WeekdayName|Year|CVar|Val|Write|Redirect|MapPath|HTMLEncode|URLEncode|Open|Close|Execute|AddNew|Update|MoveNext|MoveFirst|MoveLast|MovePrevious)$"))
 
 ; === Strings ===
 (string) @string
 
 ; === Numbers ===
 (number) @number
-
-; === Boolean / Special literals (matched as identifiers) ===
-((identifier) @constant.builtin
-  (#match? @constant.builtin "^(?i)(True|False|Null|Nothing|Empty)$"))
 
 ; === Comments ===
 (comment) @comment
@@ -45,8 +51,8 @@
 
 ; === #include directive ===
 (include_directive
-  "#include" @keyword.import
-  (string) @string.special)
+  "#include" @keyword
+  (string) @string)
 
 ; === Plain identifiers (variables, function/sub names, everything else) ===
 (identifier) @variable
